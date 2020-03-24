d2c1bc22cef36ba8b359d1bc7335560bdf2cc96d
cesarvin <vinizio007@gmail.com>
2020-03-24 01:59:30 -0600

    app

---
+++ src/app/_services/authentication.service.ts
@@ -0,0 +1,43 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { User } from '@/_models';
+
+@Injectable({ providedIn: 'root' })
+export class AuthenticationService {
+    private currentUserSubject: BehaviorSubject<User>;
+    public currentUser: Observable<User>;
+
+    constructor(private http: HttpClient) {
+        this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
+        this.currentUser = this.currentUserSubject.asObservable();
+    }
+
+    public get currentUserValue(): User {
+        return this.currentUserSubject.value;
+    }
+
+    login(user, pass) {
+        return this.http.post<any>('http://localhost:3000/login', { user, pass })
+            .pipe(map(login => {
+              console.log('login->', login);  
+              if (login.login == 1){
+                localStorage.setItem('currentUser', login.login);
+                localStorage.setItem('menu', JSON.stringify(login.menu));
+              }else {
+
+              }
+              return login.menu;
+          }));
+    }
+
+    
+
+    logout() {
+        // remove user from local storage and set current user to null
+        localStorage.removeItem('currentUser');
+        this.currentUserSubject.next(null);
+    }
+}
